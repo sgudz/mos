260d33a0d6513fac5fd5a9d143ff11b930b66f7f
vortex610 <vortexuelpm@gmail.com>
2016-08-17 12:07:12 +0300

    Update addresult_wally.py

--- run_tests/shaker_run/addresult_wally.py
+++ run_tests/shaker_run/addresult_wally.py
@@ -148,12 +148,12 @@ client.send_post('add_result/{}'.format(test_4kib_read),
                           'custom_baseline_throughput': int(base_read_4kib_median),
                           'custom_baseline_stdev': int(base_read_4kib_stdev)})
 client.send_post('add_result/{}'.format(test_4kib_write),
-                         {'status_id': int(write_4kib_status), 'version': str(version), 'custom_throughput': int(write_4kib_median),
+                         {'status_id': write_4kib_status, 'version': str(version), 'custom_throughput': int(write_4kib_median),
                           'custom_stdev': int(write_4kib_stdev),
                           'custom_baseline_throughput': int(base_write_4kib_median),
                           'custom_baseline_stdev': int(base_write_4kib_stdev)})
 client.send_post('add_result/{}'.format(test_16mib_read),
-                         {'status_id': int(read_16mib_status), 'version': str(version), 'custom_throughput': int(read_16mib_median),
+                         {'status_id': read_16mib_status, 'version': str(version), 'custom_throughput': int(read_16mib_median),
                           'custom_stdev': int(read_16mib_stdev),
                           'custom_baseline_throughput': int(base_read_16mib_median),
                           'custom_baseline_stdev': int(base_read_16mib_stdev)})
