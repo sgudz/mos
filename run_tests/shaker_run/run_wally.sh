ec52ef82f8615c97904762ed1523cced93b524af
vortex610 <vortexuelpm@gmail.com>
2016-08-18 14:41:59 +0300

    Update run_wally.sh

--- run_tests/shaker_run/run_wally.sh
+++ run_tests/shaker_run/run_wally.sh
@@ -10,8 +10,8 @@ set -x
 source /root/openrc
 sleep 1
 #IMAGE=$(glance image-list | awk '/wally_ubuntu/ {print $2}' | head -n 1)
-IMAGE=$(glance image-list | grep wally_ubuntu | head -n 1 | awk '{print $4}')
-if [ -z $IMAGE ];then
+IMAGE=$(glance image-list | grep wally_ubuntu | head -n 1)
+if [ -z ${IMAGE} ];then
 wget --quiet "https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img"
 glance image-create --name wally_ubuntu --disk-format qcow2 --container-format bare --visibility public --file "trusty-server-cloudimg-amd64-disk1.img"
 rm /root/trusty-server-cloudimg-amd64-disk1.img
