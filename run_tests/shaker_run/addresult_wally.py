8cec8d5df84bdba1f2a396857eb015305a055d91
vortex610 <vortexuelpm@gmail.com>
2016-08-19 12:27:27 +0300

    Update addresult_wally.py

--- run_tests/shaker_run/addresult_wally.py
+++ run_tests/shaker_run/addresult_wally.py
@@ -2,7 +2,7 @@ import ConfigParser
 import base64
 import json
 import urllib2
-from run_tests.shaker_run.plugin.testrailplugin import *
+from run_tests.shaker_run.plugin import *
 
 # # Testrail API
 # class APIClient:
