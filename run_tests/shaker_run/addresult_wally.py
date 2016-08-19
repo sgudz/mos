3c37567c19c799c79b3d645fdc285291e9671e43
vortex610 <vortexuelpm@gmail.com>
2016-08-19 12:25:30 +0300

    Update addresult_wally.py

--- run_tests/shaker_run/addresult_wally.py
+++ run_tests/shaker_run/addresult_wally.py
@@ -2,56 +2,56 @@ import ConfigParser
 import base64
 import json
 import urllib2
+from run_tests.shaker_run.plugins.testrailplugin import *
+
+# # Testrail API
+# class APIClient:
+#     def __init__(self, base_url):
+#         self.user = ''
+#         self.password = ''
+#         if not base_url.endswith('/'):
+#             base_url += '/'
+#         self.__url = base_url + 'index.php?/api/v2/'
+
+#     def send_get(self, uri):
+#         return self.__send_request('GET', uri, None)
+
+#     def send_post(self, uri, data):
+#         return self.__send_request('POST', uri, data)
+
+#     def __send_request(self, method, uri, data):
+#         url = self.__url + uri
+#         request = urllib2.Request(url)
+#         if (method == 'POST'):
+#             request.add_data(json.dumps(data))
+#         auth = base64.b64encode('%s:%s' % (self.user, self.password))
+#         request.add_header('Authorization', 'Basic %s' % auth)
+#         request.add_header('Content-Type', 'application/json')
+
+#         e = None
+#         try:
+#             response = urllib2.urlopen(request).read()
+#         except urllib2.HTTPError as e:
+#             response = e.read()
+
+#         if response:
+#             result = json.loads(response)
+#         else:
+#             result = {}
+
+#         if e != None:
+#             if result and 'error' in result:
+#                 error = '"' + result['error'] + '"'
+#             else:
+#                 error = 'No additional error message received'
+#             raise APIError('TestRail API returned HTTP %s (%s)' %
+#                           (e.code, error))
+
+#         return result
 
 
-# Testrail API
-class APIClient:
-    def __init__(self, base_url):
-        self.user = ''
-        self.password = ''
-        if not base_url.endswith('/'):
-            base_url += '/'
-        self.__url = base_url + 'index.php?/api/v2/'
-
-    def send_get(self, uri):
-        return self.__send_request('GET', uri, None)
-
-    def send_post(self, uri, data):
-        return self.__send_request('POST', uri, data)
-
-    def __send_request(self, method, uri, data):
-        url = self.__url + uri
-        request = urllib2.Request(url)
-        if (method == 'POST'):
-            request.add_data(json.dumps(data))
-        auth = base64.b64encode('%s:%s' % (self.user, self.password))
-        request.add_header('Authorization', 'Basic %s' % auth)
-        request.add_header('Content-Type', 'application/json')
-
-        e = None
-        try:
-            response = urllib2.urlopen(request).read()
-        except urllib2.HTTPError as e:
-            response = e.read()
-
-        if response:
-            result = json.loads(response)
-        else:
-            result = {}
-
-        if e != None:
-            if result and 'error' in result:
-                error = '"' + result['error'] + '"'
-            else:
-                error = 'No additional error message received'
-            raise APIError('TestRail API returned HTTP %s (%s)' %
-                           (e.code, error))
-
-        return result
-
-
-class APIError(Exception):
-    pass
+# class APIError(Exception):
+#     pass
 
 
 client = APIClient('https://mirantis.testrail.com/')
