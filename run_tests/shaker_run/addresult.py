5e0b3e9b4af446c4f6153eaca2427e05895b05fb
vortex610 <vortexuelpm@gmail.com>
2016-09-06 17:45:34 +0300

    Update addresult.py

--- run_tests/shaker_run/addresult.py
+++ run_tests/shaker_run/addresult.py
@@ -216,6 +216,7 @@ elif l3ha and vlan and offloading:
 else:
     #print "Wrong cluster config. DVR: {0}, L3HA: {1}, VLAN: {2}, VXLAN: {3}, BETWEEN_NODES: {4}, OFFLOADING: {5}".format(dvr, l3ha, vlan, vxlan, between_nodes, offloading)
     raise ClusterError("DVR: {0}, L3HA: {1}, VLAN: {2}, VXLAN: {3}, BETWEEN_NODES: {4}, OFFLOADING: {5}".format(dvr, l3ha, vlan, vxlan, between_nodes, offloading))
+    exit 1
     #test_id = test8
 print "Test ID for testing: {}".format(test_id)
 print "DVR: {0}, L3HA: {1}, VLAN: {2}, VXLAN: {3}, BETWEEN_NODES: {4}, OFFLOADING: {5}".format(dvr, l3ha, vlan, vxlan, between_nodes, offloading)
