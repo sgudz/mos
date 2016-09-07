7dd74f825227bb8527caf630f045a14f5d6b44d8
vortex610 <vortexuelpm@gmail.com>
2016-09-07 15:25:50 +0300

    Update addresult.py

--- run_tests/shaker_run/addresult.py
+++ run_tests/shaker_run/addresult.py
@@ -162,86 +162,87 @@ def get_computes(fuel_ip, token_id):
 #                 state = True
 #             offloading_nodes["Node-" + str(node)] = state
 #     return offloading_nodes
-token_id = get_token_id(fuel_ip)
 
-median = 0
-stdev = 0
-run_id = get_run_id()
-test1, test2, test3, test4, test5, test6, test7, test8 = get_tests_ids(run_id)
-seg_type = get_neutron_conf(fuel_ip, token_id)['networking_parameters']['segmentation_type']
-if seg_type == 'vlan':
-    vlan = True
-    vxlan = False
-else:
-    vlan = False
-    vxlan = True
-dvr = get_cluster_attributes(fuel_ip, token_id)['editable']['neutron_advanced_configuration']['neutron_dvr']['value']
-l3ha = get_cluster_attributes(fuel_ip, token_id)['editable']['neutron_advanced_configuration']['neutron_l3_ha']['value']
-nodes = get_nodes(fuel_ip, token_id)
-compute_id1 = get_computes(fuel_ip, token_id)[0]
-compute_id2 = get_computes(fuel_ip, token_id)[1]
-# offloading_compute1 = get_offloading(fuel_ip, token_id)['Node-{}'.format(compute_id1)]
-# offloading_compute2 = get_offloading(fuel_ip, token_id)['Node-{}'.format(compute_id2)]
-# if offloading_compute1 and offloading_compute2:
-#     offloading = True
-# elif not offloading_compute1 and not offloading_compute2:
-#     offloading = False
-# else:
-#     offloading = "Unknown"
-        
-offloading = True
-if dvr and vxlan and offloading:
-    test_id = test3
-elif dvr and vlan and offloading:
-    test_id = test4
-elif dvr and vxlan:
-    test_id = test1
-elif dvr and vlan:
-    test_id = test2
-elif l3ha and vxlan and offloading and between_nodes:
-    test_id = test5
-elif l3ha and vxlan and offloading:
-    test_id = test6
-elif l3ha and vlan and offloading and between_nodes:
-    test_id = test7
-elif l3ha and vlan and offloading:
-    test_id = test8
-else:
-    #print "Wrong cluster config. DVR: {0}, L3HA: {1}, VLAN: {2}, VXLAN: {3}, BETWEEN_NODES: {4}, OFFLOADING: {5}".format(dvr, l3ha, vlan, vxlan, between_nodes, offloading)
-    #raise ClusterError("DVR: {0}, L3HA: {1}, VLAN: {2}, VXLAN: {3}, BETWEEN_NODES: {4}, OFFLOADING: {5}".format(dvr, l3ha, vlan, vxlan, between_nodes, offloading))
-    #sys.exit("Wrong cluster config")
-    test_id = test8
-
-base_median = client.send_get('get_test/{}'.format(test_id))['custom_test_case_steps'][0]['expected']
-base_stdev = client.send_get('get_test/{}'.format(test_id))['custom_test_case_steps'][1]['expected']
-
-
-
-print "Test ID for testing: {}".format(test_id)
-print "DVR: {0}, L3HA: {1}, VLAN: {2}, VXLAN: {3}, BETWEEN_NODES: {4}, OFFLOADING: {5}".format(dvr, l3ha, vlan, vxlan, between_nodes, offloading)
-content = dict(parser.items('test_json'))['json_data']
-json_data = json.loads(content)
-item = [each for each in json_data['records']]
-for i in range(len(item)):
-    try:
-        median = int(round(json_data['records'][item[i]]['stats']['bandwidth']['median'], 0))
-        stdev = int(round(json_data['records'][item[i]]['stats']['bandwidth']['stdev'], 0))
-    except KeyError:
-        continue
-
-test_glob_status = test_custom_median_status = test_custom_stdev_status = 1
-if median < float(base_median) * 0.9:
-    test_glob_status = test_custom_median_status = 5
-if stdev < float(base_stdev) * 0.5:
-    test_custom_stdev_status = 5
-
-### Collecting results
-custom_test_res = [{'status_id': test_custom_median_status, 'content': 'Check [network bandwidth, Median; Mbps]',
-                         'expected': str(base_median), 'actual': str(median)},
-                        {'status_id': test_custom_stdev_status, 'content': 'Check [deviation; pcs]', 'expected': str(base_stdev),
-                         'actual': str(stdev)}]
-glob_test_result = {'test_id': test_id, 'status_id': test_glob_status, 'version': str(version),
-                 'custom_test_case_steps_results': custom_test_res}
-
-results_all_dict = {'results': [glob_test_result]}
-client.send_post('add_results/{}'.format(run_id), results_all_dict)
+if __name__ == "__main__":
+    token_id = get_token_id(fuel_ip)
+    median = 0
+    stdev = 0
+    run_id = get_run_id()
+    test1, test2, test3, test4, test5, test6, test7, test8 = get_tests_ids(run_id)
+    seg_type = get_neutron_conf(fuel_ip, token_id)['networking_parameters']['segmentation_type']
+    if seg_type == 'vlan':
+        vlan = True
+        vxlan = False
+    else:
+        vlan = False
+        vxlan = True
+    dvr = get_cluster_attributes(fuel_ip, token_id)['editable']['neutron_advanced_configuration']['neutron_dvr']['value']
+    l3ha = get_cluster_attributes(fuel_ip, token_id)['editable']['neutron_advanced_configuration']['neutron_l3_ha']['value']
+    nodes = get_nodes(fuel_ip, token_id)
+    compute_id1 = get_computes(fuel_ip, token_id)[0]
+    compute_id2 = get_computes(fuel_ip, token_id)[1]
+    # offloading_compute1 = get_offloading(fuel_ip, token_id)['Node-{}'.format(compute_id1)]
+    # offloading_compute2 = get_offloading(fuel_ip, token_id)['Node-{}'.format(compute_id2)]
+    # if offloading_compute1 and offloading_compute2:
+    #     offloading = True
+    # elif not offloading_compute1 and not offloading_compute2:
+    #     offloading = False
+    # else:
+    #     offloading = "Unknown"
+            
+    offloading = True
+    if dvr and vxlan and offloading:
+        test_id = test3
+    elif dvr and vlan and offloading:
+        test_id = test4
+    elif dvr and vxlan:
+        test_id = test1
+    elif dvr and vlan:
+        test_id = test2
+    elif l3ha and vxlan and offloading and between_nodes:
+        test_id = test5
+    elif l3ha and vxlan and offloading:
+        test_id = test6
+    elif l3ha and vlan and offloading and between_nodes:
+        test_id = test7
+    elif l3ha and vlan and offloading:
+        test_id = test8
+    else:
+        #print "Wrong cluster config. DVR: {0}, L3HA: {1}, VLAN: {2}, VXLAN: {3}, BETWEEN_NODES: {4}, OFFLOADING: {5}".format(dvr, l3ha, vlan, vxlan, between_nodes, offloading)
+        #raise ClusterError("DVR: {0}, L3HA: {1}, VLAN: {2}, VXLAN: {3}, BETWEEN_NODES: {4}, OFFLOADING: {5}".format(dvr, l3ha, vlan, vxlan, between_nodes, offloading))
+        #sys.exit("Wrong cluster config")
+        test_id = test8
+    
+    base_median = client.send_get('get_test/{}'.format(test_id))['custom_test_case_steps'][0]['expected']
+    base_stdev = client.send_get('get_test/{}'.format(test_id))['custom_test_case_steps'][1]['expected']
+    
+    
+    
+    print "Test ID for testing: {}".format(test_id)
+    print "DVR: {0}, L3HA: {1}, VLAN: {2}, VXLAN: {3}, BETWEEN_NODES: {4}, OFFLOADING: {5}".format(dvr, l3ha, vlan, vxlan, between_nodes, offloading)
+    content = dict(parser.items('test_json'))['json_data']
+    json_data = json.loads(content)
+    item = [each for each in json_data['records']]
+    for i in range(len(item)):
+        try:
+            median = int(round(json_data['records'][item[i]]['stats']['bandwidth']['median'], 0))
+            stdev = int(round(json_data['records'][item[i]]['stats']['bandwidth']['stdev'], 0))
+        except KeyError:
+            continue
+    
+    test_glob_status = test_custom_median_status = test_custom_stdev_status = 1
+    if median < float(base_median) * 0.9:
+        test_glob_status = test_custom_median_status = 5
+    if stdev < float(base_stdev) * 0.5:
+        test_custom_stdev_status = 5
+    
+    ### Collecting results
+    custom_test_res = [{'status_id': test_custom_median_status, 'content': 'Check [network bandwidth, Median; Mbps]',
+                             'expected': str(base_median), 'actual': str(median)},
+                            {'status_id': test_custom_stdev_status, 'content': 'Check [deviation; pcs]', 'expected': str(base_stdev),
+                             'actual': str(stdev)}]
+    glob_test_result = {'test_id': test_id, 'status_id': test_glob_status, 'version': str(version),
+                     'custom_test_case_steps_results': custom_test_res}
+    
+    results_all_dict = {'results': [glob_test_result]}
+    client.send_post('add_results/{}'.format(run_id), results_all_dict)
