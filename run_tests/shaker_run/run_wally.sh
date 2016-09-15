7baa88abf0758a13b26d6182488180d5ed621e3e
vortex610 <vortexuelpm@gmail.com>
2016-09-15 17:33:52 +0300

    Update run_wally.sh

--- run_tests/shaker_run/run_wally.sh
+++ run_tests/shaker_run/run_wally.sh
@@ -3,7 +3,8 @@ export SSH_OPTS='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o 
 COMPUTE_IP=`fuel node | grep compute | awk -F "|" '{print $5}' | sed 's/ //g' | head -n 1`
 CONTROLLER_IP=`fuel node | grep controller | awk -F "|" '{print $5}' | sed 's/ //g' | head -n 1`
 REPL=$(ssh $CONTROLLER_IP "ceph osd dump | grep 'replicated size [23]' | head -n 1 | awk '{print \$6}'")
-echo $REPL
+echo "Replication Factor is $REPL"
+echo "repl =" $REPL >> env.conf
 ### Create image wally_ubuntu if it doesnt exist
 REMOTE_SCRIPT1=`ssh ${SSH_OPTS} $CONTROLLER_IP "mktemp"`
 ssh ${SSH_OPTS} $CONTROLLER_IP "cat > ${REMOTE_SCRIPT1}" <<EOF
@@ -21,8 +22,6 @@ EOF
 ssh ${SSH_OPTS} $CONTROLLER_IP "bash ${REMOTE_SCRIPT1}"
 
 ### Install and launch wally
-# REMOTE_SCRIPT=`ssh ${SSH_OPTS} $COMPUTE_IP "mktemp"`
-# ssh ${SSH_OPTS} $COMPUTE_IP "cat > ${REMOTE_SCRIPT}" <<EOF
 REMOTE_SCRIPT=`ssh ${SSH_OPTS} $CONTROLLER_IP "mktemp"`
 ssh ${SSH_OPTS} $CONTROLLER_IP "cat > ${REMOTE_SCRIPT}" <<EOF
 set -x
@@ -40,36 +39,10 @@ curl -s https://raw.githubusercontent.com/vortex610/mos/master/run_tests/shaker_
 curl -s https://raw.githubusercontent.com/vortex610/mos/master/run_tests/shaker_run/default.yaml > default.yaml
 python -m wally test "Fuel 9.0-rc2; perf-3 10G; ceph; repl: 3; osd: 3; bonding: off; pg_num: 1024/512" test1.yaml
 EOF
-#ssh ${SSH_OPTS} $COMPUTE_IP "bash ${REMOTE_SCRIPT}"
 ssh ${SSH_OPTS} $CONTROLLER_IP "bash ${REMOTE_SCRIPT}"
 
 scp ${SSH_OPTS} $CONTROLLER_IP:/var/wally_results/*/ceph_report.html /root/
 #ssh ${SSH_OPTS} $CONTROLLER_IP "rm -rf /var/wally_results/"
 #ssh ${SSH_OPTS} $CONTROLLER_IP "rm -rf disk_perf_test_tool/"
-READ_16MIB_MEDIAN=$(cat ceph_report.html | grep -A1 "Read" | awk '(NR == 5)' | grep -Eo "[0-9]*" | awk '(NR == 1)')
-READ_16MIB_STDEV=$(cat ceph_report.html | grep -A1 "Read" | awk '(NR == 5)' | grep -Eo "[0-9]*" | awk '(NR == 4)')
-READ_4KIB_MEDIAN=$(cat ceph_report.html | grep -A1 "Read" | awk '(NR == 2)' | grep -Eo "[0-9]*" | awk '(NR == 1)')
-READ_4KIB_STDEV=$(cat ceph_report.html | grep -A1 "Read" | awk '(NR == 2)' | grep -Eo "[0-9]*" | awk '(NR == 4)')
-
-WRITE_16MIB_MEDIAN=$(cat ceph_report.html | grep -A1 "Write" | awk '(NR == 5)' | grep -Eo "[0-9]*" | awk '(NR == 1)')
-WRITE_16MIB_STDEV=$(cat ceph_report.html | grep -A1 "Write" | awk '(NR == 5)' | grep -Eo "[0-9]*" | awk '(NR == 4)')
-WRITE_4KIB_MEDIAN=$(cat ceph_report.html | grep -A1 "Write" | awk '(NR == 2)' | grep -Eo "[0-9]*" | awk '(NR == 1)')
-WRITE_4KIB_STDEV=$(cat ceph_report.html | grep -A1 "Write" | awk '(NR == 2)' | grep -Eo "[0-9]*" | awk '(NR == 4)')
-
-LATENCY_10_IOPS=$(cat ceph_report.html | grep -PA1 "align\=\"right\"\>10\<" | awk '(NR == 2)' | grep -Eo "[0-9]*")
-LATENCY_30_IOPS=$(cat ceph_report.html | grep -PA1 "align\=\"right\"\>30\<" | awk '(NR == 2)' | grep -Eo "[0-9]*")
-LATENCY_100_IOPS=$(cat ceph_report.html | grep -PA1 "align\=\"right\"\>100\<" | awk '(NR == 2)' | grep -Eo "[0-9]*")
-echo "repl =" $REPL >> env.conf
-echo "read_16mib_median =" $READ_16MIB_MEDIAN >> env.conf
-echo "read_16mib_stdev =" $READ_16MIB_STDEV >> env.conf
-echo "read_4kib_median =" $READ_4KIB_MEDIAN >> env.conf
-echo "read_4kib_stdev =" $READ_4KIB_STDEV >> env.conf
-echo "write_16mib_median =" $WRITE_16MIB_MEDIAN >> env.conf
-echo "write_16mib_stdev =" $WRITE_16MIB_STDEV >> env.conf
-echo "write_4kib_median =" $WRITE_4KIB_MEDIAN >> env.conf
-echo "write_4kib_stdev =" $WRITE_4KIB_STDEV >> env.conf
-echo "latency_10_ms =" $LATENCY_10_IOPS >> env.conf
-echo "latency_30_ms =" $LATENCY_30_IOPS >> env.conf
-echo "latency_100_ms =" $LATENCY_100_IOPS >> env.conf
 
 python add_results_new.py
